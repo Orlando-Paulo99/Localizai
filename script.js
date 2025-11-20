e5980f080b8ac89dcca67f863350e74664ee5025
orlando-paulo99 <orlandopaulo8]7@gmail.com>
2025-11-20 11:40:44 -0300

    inicio estilo desktop e funcionalidade

---
+++ script.js
@@ -0,0 +1,25 @@
+function localizar(){
+    let cep=document.getElementById("cep").value
+
+    let  cepNew=document.getElementById("cepNew")
+    let logradouro=document.getElementById("logradouro")
+    let bairro=document.getElementById("bairro")
+    
+    try{
+        
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response=> response.json())
+        .then(data=>{
+            cepNew.innerHTML+="CEP:"+data.cep
+            logradouro.innerHTML +="Bairro:"+data.logradouro
+            bairro.innerHTML +="Bairro"+data.bairro
+        })
+
+    }
+    catch (error){
+        console.log(error)
+    }
+    
+}
+
+localizar()
